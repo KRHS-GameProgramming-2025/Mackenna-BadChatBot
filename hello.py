81f4bc38ad2d3979d07b3067036a06ee8849a088
Kenna <Mackenna.rines@kearsarge.org>
2024-09-05 08:54:39 -0400

    setup

---
+++ hello.py
@@ -0,0 +1,33 @@
+name = input('what is your name')
+print('hi ' + name)
+
+feeling = input ('how are you doing')
+if feeling == 'good':
+    print('thats good')
+elif feeling == 'okay' or feeling == 'okay':
+    print('im' + feeling + 'too')
+elif feeling == 'bad':
+    print('sorry to hear that')
+else:
+    print ('ah ok')
+    
+color = input('what is your favorite color')
+if color == 'blue':
+    print('thats my favorite color to')
+elif color == 'idk' or color == 'i dont know':
+    print('well i dont know either ' +name)
+else:
+    print(color+ ' is a nice color')
+
+food = input('what is your favorite food')
+print(food+ ' is a good food')
+if food == 'idk' or 'i dont know':
+    print(' thats ok')
+
+code = input('do you know how to code')
+if code == 'no':
+    print('thats ok most people dont know how')
+elif code == 'yes':
+    print('good for you ' + name + ' most people dont know how')
+else:
+    print('ok')
